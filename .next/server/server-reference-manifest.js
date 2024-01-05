4d8265a8cf49585cc48487fb2a7e9db3b0c4d36c
Kiss Chouraki <kchouraki@outlook.de>
2024-01-05 16:57:12 +0100

    initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BG2P/OWu2qi8aZwzNfjZdjAOcvEM/ib4MGjA0b+U5d0=\"\n}"
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+          <div className="flex flex-row">
+          <p>Dashboard Page</p>
+         </div>
+         );
+
+};
