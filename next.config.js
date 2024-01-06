2f2fbbdacf56581ff04bd30eb1f1e71fd556dd30
Kiss Chouraki <kchouraki@outlook.de>
2024-01-06 16:00:23 +0100

    configure missing env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        POSTGRES_URL: process.env.POSTGRES_URL ,
+        POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL ,
+        POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING ,
+        POSTGRES_USER: process.env.POSTGRES_USER ,
+        POSTGRES_HOST: process.env.POSTGRES_HOST ,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_DATABASE: process.env.POSTGRES_DATABASE
+    }
+};
 
 module.exports = nextConfig;
